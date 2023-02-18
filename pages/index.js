b9769156e43c2e7f9208e5a0bcaae9ff659571b0
warrenwalt <warrenwalt19@gmail.com>
2023-02-18 13:43:12 +0300

    setting up custom axios

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axiosInstance from "../lib/axios";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/products")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(data);
   return (
     <>
       On Yah
